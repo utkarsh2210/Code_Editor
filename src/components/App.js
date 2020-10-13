8d06aa647c621016427ecacf10dbe1f9664c1f1c
unknown <utkarshmaheria@gmail.com>
2020-10-13 23:06:19 +0530

    setting up editor UI with codemirror

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Editor from "./Editor";
 
 function App() {
   return (
     <>
       <div className="pane top-pane">
+        <Editor />
+        <Editor />
         <Editor />
       </div>
       <div className="pane">
