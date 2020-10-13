7130cc9603258c31a0abe9cde87e98acbd9e5d75
unknown <utkarshmaheria@gmail.com>
2020-10-13 22:45:36 +0530

    setting up the structure of the editor

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
